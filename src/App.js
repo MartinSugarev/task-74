aef21bf10af6ef1b7becafa573fc765d3a782e78
Martin <mdsugarev@gmail.com>
2022-12-07 22:51:46 +0200

    added style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
+import React, {useMemo, useState} from 'react'
 import "./App.css";
 
 function App() {
+
+const [text, setText] = useState(null);
+
+
+function check(v){
+  let r = /^\d+$/;
+  if(text){
+  if(v.match(r)){
+   return true
+  }else{
+    return false
+  }
+}
+}
+
+const boo = useMemo(() => {
+
+ return check(text)
+  
+}, [text])
+
+
+
   return (
     <div className="App">
      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
+          value={text}
           placeholder="Enter number..."
-
-        />
+          onChange={(e) => {
+            setText(e.target.value)
+          }}
+           />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
+          <i className={boo ? "fas fa-check" :"fas fa-times"} />
         </span> 
       </div>
     </div>
